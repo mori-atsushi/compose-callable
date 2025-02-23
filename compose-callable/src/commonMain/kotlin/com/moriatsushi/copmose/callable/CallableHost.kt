ab3fa753c74fe2911af32ad1e32873b541424b03
Mori Atsushi <at.sushi.tb@gmail.com>
2025-02-23 16:22:44 +0900

    Add CallableHost

---
+++ compose-callable/src/commonMain/kotlin/com/moriatsushi/copmose/callable/CallableHost.kt
@@ -0,0 +1,30 @@
+package com.moriatsushi.copmose.callable
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+
+@Composable
+fun <I, R> CallableHost(
+    state: CallableState<I, R>,
+    content: @Composable CallableHostScope<R>.(I) -> Unit,
+) {
+    val currentData = state.currentData
+    if (currentData != null) {
+        val scope = remember(currentData) { CallableHostScopeImpl(currentData) }
+        scope.content(currentData.input)
+    }
+}
+
+interface CallableHostScope<in R> {
+    /**
+     * Closes the component and returns the result to the caller.
+     * Does nothing if the component is already closed.
+     */
+    fun resume(result: R)
+}
+
+private class CallableHostScopeImpl<in R>(
+    private val data: CallableData<*, R>,
+) : CallableHostScope<R> {
+    override fun resume(result: R) = data.resume(result)
+}
