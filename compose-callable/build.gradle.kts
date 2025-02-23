55cdbff8dccb30687e8f493deb9fb7499ebb9fc2
Mori Atsushi <at.sushi.tb@gmail.com>
2025-02-23 17:31:28 +0900

    Remove some targets

--- compose-callable/build.gradle.kts
+++ compose-callable/build.gradle.kts
@@ -11,15 +11,10 @@ plugins {
 kotlin {
     androidTarget()
     jvm("desktop")
-    js(IR) {
-        browser()
-    }
     @OptIn(ExperimentalWasmDsl::class)
     wasmJs {
         browser()
     }
-    macosX64()
-    macosArm64()
     iosX64("uikitX64")
     iosArm64("uikitArm64")
     iosSimulatorArm64("uikitSimArm64")
